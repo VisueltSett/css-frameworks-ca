854f35131ce963942c9bfab1358f560b28af969e
VisueltSett_AneTh <ane@visueltsett.no>
2021-04-11 22:03:58 +0200

    edits styles

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './sass/style.scss';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 function App() {
   return (
     <>
+    <div className="wrapper">
       <Navigation />
       <Footer />
+      </div>
     </>
 );
 }
