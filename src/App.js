413f28d5636a825fed45c6812851ce40c174a2ac
VisueltSett_AneTh <ane@visueltsett.no>
2021-04-11 19:22:13 +0200

    edits

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './sass/style.scss';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
